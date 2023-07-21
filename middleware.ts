5663470f731ece26c7c53096af9ed021b4bf084a
shriyanshu raj <108788182+rajshriyanhu@users.noreply.github.com>
2023-07-21 17:09:18 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ['/',/\/products\/[a-zA-Z0-9-]+/,/\/category\/[a-zA-Z0-9-]+/],
-  ignoredRoutes: ["/studio/desk/product;"]
+  ignoredRoutes: ["/studio/desk/product"]
 });
 
 export const config = {
